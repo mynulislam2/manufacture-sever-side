bf01f4de7758d1cb2de579ba912eb4d63eb875fc
mynul <homeexercise8@gmail.com>
2022-05-26 23:00:39 +0600

    some comment

--- index.js
+++ index.js
@@ -123,6 +123,7 @@ async function run() {
             const result = await BookingCollection.deleteOne(query);
             res.send(result)
         })
+        // pay 
         app.get('/pay/:id', verifyJWT, async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) };
